71a1662535d4c85860f2b84f405eb2cfd69c2775
Patrick cheruiyot <35229118+tricrck@users.noreply.github.com>
2025-11-08 21:41:17 +0300

    Refactor MyDatabaseContext by removing unused code
    Removed unused value converter for DateTime and adjusted formatting.

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -1,28 +1,15 @@
+#nullable disable
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System;
 
 namespace DotNetCoreSqlDb.Models
 {
     public class MyDatabaseContext : DbContext
     {
-        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options)
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
             : base(options)
-        { }
-
-        public DbSet<Todo> Todo { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var converter = new ValueConverter<DateTime, string>(
-                v => v.ToString("yyyy-MM-dd HH:mm:ss"), // write as string
-                v => DateTime.Parse(v)                  // read back as DateTime
-            );
-
-            modelBuilder.Entity<Todo>()
-                .Property(t => t.CreatedDate)
-                .HasConversion(converter)
-                .HasColumnType("TEXT"); // matches your SQLite TEXT column
         }
+
+        public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
     }
 }
